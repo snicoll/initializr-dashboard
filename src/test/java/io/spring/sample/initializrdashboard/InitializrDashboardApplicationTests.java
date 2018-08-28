c1c655076b50f4ea3d087706c5c0267d263f61a6
Brian Clozel <bclozel@pivotal.io>
2018-08-28 11:02:56 +0200

    Create project with start.spring.io

---
+++ src/test/java/io/spring/sample/initializrdashboard/InitializrDashboardApplicationTests.java
@@ -0,0 +1,16 @@
+package io.spring.sample.initializrdashboard;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InitializrDashboardApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
